29934f239c2999feb0dfaf9213d8b318ad235c92
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-11-26 13:37:59 -0800

    Create Constants.h

---
+++ src/Constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <vector>
+#include <string>
+
+namespace Constants
+{
+    const std::vector<std::string> SupportedFormats = {
+        "%Y-%m-%d", "%d-%m-%Y", "%m/%d/%Y", "%B %d, %Y"};
+}
+
+#endif
